65371a44478fb4ec44e6c5b82462bd341eee8d3a
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2023-03-15 08:37:32 +0100

    first commit

---
+++ src/main/java/com/jaimecorg/facultad/FacultadApplication.java
@@ -0,0 +1,13 @@
+package com.jaimecorg.facultad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultadApplication.class, args);
+	}
+
+}
